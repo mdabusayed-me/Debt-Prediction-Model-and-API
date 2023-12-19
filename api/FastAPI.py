f3c4a50fb6fef923fc6b61317b29165f387fde3b
mdabusayed-me <asayed106@gmail.com>
2023-12-19 21:57:51 +0600

    FastAPI update

--- api/FastAPI.py
+++ api/FastAPI.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import mysql.connector
 import uvicorn
 import numpy as np 
 import pickle
 import pandas as pd
 import requests
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="debt_db2"
+)
 
 
 app = FastAPI()
@@ -12,6 +20,23 @@ app = FastAPI()
 def index():
     return{"hellow": "FastAPI"}
 
+# Get all loan_data
+@app.get("/loan_data")
+def get_loan_data():
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM loan_data")
+    result = cursor.fetchall()
+    return {"loan_data": result}
+
+# Add a new loan data
+@app.post("/loan_data")
+def add_loan_data(Loan_ID: str, Gender: int, Married: str, Dependents: int, Education: str, Self_Employed: str, ApplicantIncome: int, CoapplicantIncome: int, LoanAmount: int, Loan_Amount_Term: int, Credit_History: int,  Property_Area: str, Loan_Status: str):
+    cursor = mydb.cursor()
+    sql = "INSERT INTO loan_data (Loan_ID, Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History, Property_Area, Loan_Status) VALUES (%s, %s, %s, %s ,%s, %s ,%s, %s ,%s, %s ,%s, %s, %s)"
+    val = (Loan_ID, Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History, Property_Area, Loan_Status)
+    cursor.execute(sql, val)
+    mydb.commit()
+    return {"message": "Loan_data added successfully"}
 
 
 if __name__ == "__main__":
