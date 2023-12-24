a289eb141a2ca5a76f5525d7b54838a6f8142244
Jubair <jubair@metadesignsolutions.co.uk>
2023-12-24 17:51:17 +0600

    data adding api commented

--- modules/loan/loan.py
+++ modules/loan/loan.py
@@ -13,15 +13,15 @@ class DropdownResponse(BaseModel):
 
 connection, cursor, db = db_connection.get_db()
 
-@router.post("/add_loan_train_data")
-async def add_loan(Gender: str, Married: str, Dependents: int, Education: str, Self_Employed: str, ApplicantIncome: float, CoapplicantIncome: float, LoanAmount: float, Loan_Amount_Term: int, Credit_History: float, Property_Area: str, Loan_Status: str):
-    cursor = connection.cursor()
-    query = "INSERT INTO loans (Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History, Property_Area, Loan_Status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History, Property_Area, Loan_Status)
-    cursor.execute(query, values)
-    connection.commit()
-    cursor.close()
-    return {"message": "Loan added successfully"}
+# @router.post("/add_loan_train_data")
+# async def add_loan(Gender: str, Married: str, Dependents: int, Education: str, Self_Employed: str, ApplicantIncome: float, CoapplicantIncome: float, LoanAmount: float, Loan_Amount_Term: int, Credit_History: float, Property_Area: str, Loan_Status: str):
+#     cursor = connection.cursor()
+#     query = "INSERT INTO loans (Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History, Property_Area, Loan_Status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+#     values = (Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History, Property_Area, Loan_Status)
+#     cursor.execute(query, values)
+#     connection.commit()
+#     cursor.close()
+#     return {"message": "Loan added successfully"}
 
 
 @router.get("/loan_train_data")
