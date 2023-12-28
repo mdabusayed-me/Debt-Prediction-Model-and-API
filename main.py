ec742a30601255acaf7abcb276ae3eac9054a9cc
Jubair <jubair@metadesignsolutions.co.uk>
2023-12-28 18:52:27 +0600

    new dataset pkl added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from modules.router import api_router
+import uvicorn
 
 app = FastAPI()
 
@@ -8,3 +9,6 @@ def index():
     return{"hello": "FastAPI is working"}
 
 app.include_router(api_router, prefix='')
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True, port=8000, host="0.0.0.0")
