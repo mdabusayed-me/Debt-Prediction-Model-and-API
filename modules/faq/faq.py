01e7e5960a3713464895d2edbf3a5b057f6a2855
Jubair <jubair@metadesignsolutions.co.uk>
2024-03-28 01:40:44 +0600

    refactor

--- modules/faq/faq.py
+++ modules/faq/faq.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import sys
-from fastapi import APIRouter,Body, Path
+from fastapi import APIRouter,Body, Path, FastAPI, HTTPException
 from llama_index.core import (ServiceContext, SimpleDirectoryReader,
                               StorageContext, VectorStoreIndex,
                               load_index_from_storage)
@@ -20,6 +20,8 @@ os.environ["OPENAI_API_KEY"] = env['OPENAI_API_KEY']
 openai.api_key = env['OPENAI_API_KEY']
 
 router = APIRouter()
+
+chat_engines = {}
 
 @router.post("/ask")
 async def ask_question(question: str = Body(...)):
@@ -46,4 +48,56 @@ async def ask_question(question: str = Body(...)):
     return {"data": response.response}
 
 
+# def create_chat_engine(indexStorage: any):
+#     global chat_engines
+#     chat_id = str(uuid.uuid4()) 
 
+#     memory = ChatMemoryBuffer.from_defaults(token_limit=2000)
+
+#     chat_engine = indexStorage.as_chat_engine(
+#         chat_mode="context",
+#         memory=memory,
+#         system_prompt=(
+#             "You are a chatbot, able to have normal interactions, as well as talk"
+#             # " about an essay discussing Paul Grahams life."
+#         ),
+#     )
+
+#     chat_engines[chat_id] = chat_engine
+
+#     return chat_id
+
+
+
+# @router.post("/create_chat")
+# async def create_chat(transcript_id: str):
+
+#     if not transcript_id:
+#         raise HTTPException(status_code=400, detail="transcript_id is required")
+#     transcription = transcript_collection.find_one(ObjectId(transcript_id))
+#     if not transcription:
+#         raise HTTPException(status_code=404, detail="Chat not found")
+
+#     transcription["_id"] = str(transcription["_id"])
+
+#     index = create_index_and_query(transcript_id, transcription["transcription"])
+    
+#     chat_id = create_chat_engine(index)
+
+#     return {"chat_id": chat_id, "transcript_id": transcript_id}
+
+
+# @router.post('/history_chat')
+# async def history_chat(payload: Agent2QuestionRequest):
+#     global chat_engines
+
+#     if not payload.chat_id:
+#         raise HTTPException(status_code=400, detail="Chat ID is required")
+
+#     # Check if the chat engine is already loaded
+#     if payload.chat_id not in chat_engines:
+#         return 'Chat not found'
+    
+#     response = chat_engines[payload.chat_id].chat(payload.question)
+
+#     return response
